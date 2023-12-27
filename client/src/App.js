998ce16cd39bec11ea9fd520ff5b413733a19383
Kawayan <kawayan.deguzman@gmail.com>
2023-12-26 21:30:20 -0800

    create post div

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,21 @@ function App() {
           <a href="">Register</a>
         </nav>
       </header>
+      <div className="post">
+        <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1" alt=""></img>
+        <h2>EcoFlow teases full-house battery backup coming later this year</h2>
+        <p>Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
+      </div>
+      <div className="post">
+        <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1" alt=""></img>
+        <h2>EcoFlow teases full-house battery backup coming later this year</h2>
+        <p>Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
+      </div>
+      <div className="post">
+        <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1" alt=""></img>
+        <h2>EcoFlow teases full-house battery backup coming later this year</h2>
+        <p>Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
+      </div>
     </main>
   );
 }
