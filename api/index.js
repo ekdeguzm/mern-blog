63ed1b04dc4e441ebc5d7e9a4c73052e27401a2a
Kawayan <kawayan.deguzman@gmail.com>
2023-12-28 14:30:39 -0800

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
-const User = require('./models/User')
+const User = require('/models/User');
 const app = express();
 
 app.use(cors());
