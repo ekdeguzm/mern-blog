980c469130643bc872ede189346486be128014fa
Kawayan <kawayan.deguzman@gmail.com>
2023-12-28 01:24:19 -0800

    style login and register pages

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,6 +1,7 @@
 export default function LoginPage() {
   return( 
     <form className="login">
+      <h1>Login</h1>
       <input type="text" placeholder="username"></input>
       <input type="text" placeholder="password"></input>
       <button>Login</button>
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,9 +1,10 @@
 export default function RegisterPage() {
   return(
     <form className="register">
+      <h1>Register</h1>
       <input type="text" placeholder="username"></input>
       <input type="text" placeholder="password"></input>
-      <button>Login</button>
+      <button>Register</button>
   </form>
   )
 }
