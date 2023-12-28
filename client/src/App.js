bd7049422f324704734b2febe90a51053a4872be
Kawayan <kawayan.deguzman@gmail.com>
2023-12-27 17:23:18 -0800

    create login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import Post from "./Post";
 import Header from "./Header";
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <main>
-    <Header />
-    <Post />
-    <Post />
-    <Post />
-    </main>
+    <Routes>
+      <Route index element={
+        <main>
+          <Header />
+          <Post />
+          <Post />
+          <Post />
+        </main>
+      } />
+      <Route path={'/login'} element={
+        <div>login</div>
+      } />
+    </Routes>
   );
 }
 
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return(
-  <header>
-    <a href="" className="logo">My Blog</a>
-    <nav>
-      <a href="">Login</a>
-      <a href="">Register</a>
-    </nav>
-</header>
+    <header>
+      <a href="" className="logo">My Blog</a>
+      <nav>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </nav>
+    </header>
   )
 }
