e43b67ff9e2e8e9d93f83cbc06ad46980cbe96de
Kawayan <kawayan.deguzman@gmail.com>
2023-12-28 12:56:52 -0800

    create backend for register page

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,9 +1,28 @@
+import { useState } from "react";
+
 export default function RegisterPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  async function register(ev) {
+    ev.preventDefault();
+    fetch('http://localhost:4000', {
+      method: 'POST',
+      body: JSON.stringify({username,password}),
+      headers: {'Content-Type':'application/json'},
+    })
+  }
   return(
-    <form className="register">
+    <form className="register" onSubmit={register}>
       <h1>Register</h1>
-      <input type="text" placeholder="username"></input>
-      <input type="text" placeholder="password"></input>
+      <input type="text" 
+        placeholder="username"
+        value={username}
+        onChange={ev => setUsername(ev.target.value)}></input>
+      <input type="text" 
+        placeholder="password"
+        value={password}
+        onChange={ev => setPassword(ev.target.value)}></input>
+
       <button>Register</button>
   </form>
   )
