54e0c8c9ff7c274332856baab4742195cb498fdd
Kawayan <kawayan.deguzman@gmail.com>
2023-12-28 14:58:59 -0800

    add mongo

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
-const User = require('/models/User');
+const User = require('./models/user');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://blog:LxzeTXwLdSyswZwX@cluster0.tftoudk.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://emdeguzm:WapaArIidRiaw2e4@cluster0.it3b2me.mongodb.net/?retryWrites=true&w=majority');
 
 app.post('/register', async (req,res) => {
   const {username,password} = req.body;
@@ -18,4 +18,7 @@ app.post('/register', async (req,res) => {
 app.listen(4000);
 
 
-
+//mongodb+srv://blog:<password>@cluster0.tftoudk.mongodb.net/?retryWrites=true&w=majority
+//emdeguzm
+//WapaArIidRiaw2e4
+//mongodb+srv://emdeguzm:WapaArIidRiaw2e4@cluster0.it3b2me.mongodb.net/?retryWrites=true&w=majority
